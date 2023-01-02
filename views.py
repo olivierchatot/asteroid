483e967f2c16a0107caff4e718c88ea3c2311dba
Olivier Chatot <chatotolivier@gmail.com>
2023-01-02 12:44:44 -0700

    Colors on top-10 scores

--- views.py
+++ views.py
@@ -10,6 +10,12 @@ class Asteroid(View):
     def get(self, request):
         context = {}
         context['high_scores'] = HighScore.objects.order_by("-score")[:10]
+        for index, score in enumerate(context['high_scores']):
+            if index == 0: score.style = "color: #33DAFF; font-weight: bold; font-size: 30px;"
+            elif index == 1: score.style = "color: #FFD700; font-weight: bold;"
+            elif index == 2: score.style = "color: #C9C0BB; font-weight: bold;"
+            elif index == 3: score.style = "color: #CD7F32; font-weight: bold;"
+            else: score.style = "color: #000;"
         return render(request, 'asteroid/index.html', context)
 
     def post(self, request):
